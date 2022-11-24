ce91b83b48dec8b542d3a59804f2908cc7e7b9f5
elyngoms <elygnoms@gmail.com>
2022-11-24 09:41:09 -0500

    bringing over earlier code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
+  const [color, setColor] = React.useState(
+    JSON.parse(sessionStorage.getItem("color")) || "blue"
+  ); //using hooks for function component
+  const [count, setCount] = React.useState(
+    JSON.parse(localStorage.getItem("count")) || 0
+  );
+  console.log(color);
+  console.log(count);
+
+  React.useEffect(() => {
+    sessionStorage.setItem("color", JSON.stringify(color));
+    localStorage.setItem("count", JSON.stringify(count));
+  }, [color, count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* this component has 2 children that display a message and a clock*/}
+      <DisplayMessage color={color} />
+      <Clock time={new Date().toLocaleTimeString()} />
+      <p>{count}</p>
+      <button
+        onClick={() => {
+          setColor(toggle(color));
+          setCount(count + 1);
+        }}
+      >
+        Change Color React
+      </button>
     </div>
   );
 }
+function toggle(color) {
+  if (color === "blue") {
+    return "red";
+  } else if (color === "red") {
+    return "green";
+  } else if (color === "green") {
+    return "blue";
+  }
+}
+
+function DisplayMessage(props) {
+  return <h1 style={{ color: props.color }}>Hello React World! </h1>;
+}
+
+function Clock(props) {
+  return <p> React Clock: {props.time} </p>;
+}
+//const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(<Main />); //first load
+//setInterval(() => root.render(<Main />), 1000);
 
 export default App;
