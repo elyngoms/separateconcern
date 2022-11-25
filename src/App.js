5039f171321e31a4e0071524ed11ce2ac03092a5
elyngoms <elygnoms@gmail.com>
2022-11-25 09:50:54 -0500

    react rout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
-import Main from "./components/Main";
+//import Main from "./components/Main";
+
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+//import Header from "./components/Header";
 
 function App() {
-  return <Main />;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
 }
 export default App;
