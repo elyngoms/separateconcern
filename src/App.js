df9abb12c5382d77c7c91edb0b97511ea68852d3
elyngoms <elygnoms@gmail.com>
2022-11-25 10:59:39 -0500

    shared layout for routes

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Header from "./components/Header";
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
   return (
     <div>
-      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
       </Routes>
     </div>
   );
