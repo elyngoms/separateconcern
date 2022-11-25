6a7dafe63b057639482a166d17f879318e168bdb
elyngoms <elygnoms@gmail.com>
2022-11-25 10:33:43 -0500

    react routing with links

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-//import Header from "./components/Header";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
